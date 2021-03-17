ab6941e5b8f63df95cb094ba9794c08a81b30eab
Monica Lara <ptts.no2020@gmail.com>
2021-03-17 09:05:26 +0100

    Commit#6- Code Smell Class UsertTest

--- src/test/java/com/patterns/behavioral/mediator/UserTest.java
+++ src/test/java/com/patterns/behavioral/mediator/UserTest.java
@@ -9,14 +9,25 @@ class UserTest {
 
     @Test
     void send() {
-        User user = new User(new Telegram(),"hola");
+        User user = new User(new Telegram(),"Juan");
+        User user2 = new User(new Telegram(),"Teresa");
         user.send("hola");
         user.mediator.sendMessage("hola",user);
+        assertNotNull(user);
 
 
     }
 
     @Test
     void receive() {
+        String sentm = "hi";
+        String receviedm = "";
+        User user = new User(new Telegram(),"Juan");
+        User user2 = new User(new Telegram(),"Teresa");
+        user.send(sentm);
+        user.mediator.sendMessage("hola",user2);
+        assertNotSame(user,user2);
+
+
     }
 }
