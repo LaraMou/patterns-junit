1ae839acdc7bbeb627e5d13ea684129423f7ee5f
Monica Lara <ptts.no2020@gmail.com>
2021-03-17 13:18:51 +0100

    Commit#15 - Clase CalculatorTest package com.patterns.testexample; Se ha añadido código de a la calse Calculator

--- src/test/java/com/patterns/testexample/CalculatorTest.java
+++ src/test/java/com/patterns/testexample/CalculatorTest.java
@@ -17,12 +17,16 @@ class CalculatorTest {
 
     @Test
     void sumTest(){
+        Calculator calc = new Calculator();
+        assertEquals(5.5,calc.sum(2.5,3));
         System.out.println("Suma");
     }
 
 
     @Test
     void subTest(){
+        Calculator calc = new Calculator();
+        assertEquals(5.5,calc.rest(10.5,5));
         System.out.println("Resta");
     }
 }
