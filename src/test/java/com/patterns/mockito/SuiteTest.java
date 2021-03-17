87db072687a43310b5cbcaaa89aaff753a4599c8
Monica Lara <ptts.no2020@gmail.com>
2021-03-17 11:37:15 +0100

    Commit#9 - suiteTest mockito se descará este error si persiste

--- src/test/java/com/patterns/mockito/SuiteTest.java
+++ src/test/java/com/patterns/mockito/SuiteTest.java
@@ -1,6 +1,16 @@
 package com.patterns.mockito;
 
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+/*
+ Ejecutaría todos los test desde esta clase que estén dentro del paquete creational e iterator y que  la
+ clase o método contenga la Tag "database"
+* */
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.patterns.mockito")
+@SuiteDisplayName("Suite patrones mockito")
 public class SuiteTest {
-
-
 }
